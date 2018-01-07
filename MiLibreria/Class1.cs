c78b79af9a7c1c2ecf6325a804495f59fc7f742f
EMILIANO\emiliano <sosaemiliano80@gmail.com>
2018-01-07 00:36:09 -0300

    mi primer commit

---
+++ MiLibreria/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace MiLibreria
+{
+    public class Utilidades
+    {
+        public static DataSet Ejecutar(string cmd)
+        {
+            SqlConnection conexion = new SqlConnection("Data Source = EMILIANO/SQLEXPRESS;Initial Catalog = Electrodomesticos;Integrated Security = True");
+            conexion.Open();
+            DataSet DS = new DataSet();
+            SqlDataAdapter DP = new SqlDataAdapter(cmd, conexion);
+            DP.Fill(DS);
+            conexion.Close();
+            return DS;
+
+            
+        }
+    }
+}
